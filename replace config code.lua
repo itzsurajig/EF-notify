a093865c61ae9862cc3addad535be6a14b9007d6
BlasterSuraj <104319683+blastersuraj@users.noreply.github.com>
2023-04-21 03:15:25 +0530

    Add files via upload

---
+++ replace config code.lua
@@ -0,0 +1,22 @@
+QBConfig.Notify.VariantDefinitions = {
+    success = {
+        classes = 'success',
+        icon = 'fab fa-facebook-messenger'
+    },
+    primary = {
+        classes = 'primary',
+        icon = 'fas fa-circle-notch fa-spin'
+    },
+    error = {
+        classes = 'error',
+        icon = 'fas fa-exclamation-circle fa-spin'
+    },
+    police = {
+        classes = 'police',
+        icon = 'fas fa-bell'
+    },
+    ambulance = {
+        classes = 'ambulance',
+        icon = 'fas fa-ambulance'
+    }
+}
